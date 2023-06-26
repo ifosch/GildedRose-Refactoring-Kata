f8d1947c58e5da1c5181f8f7923d6ec6ea35842c
Ignasi Fosch <natx@y10k.ws>
2023-06-26 19:31:17 +0200

    Isolate Backstage Passes
    Push the Backstage Passes update code into a separate method.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -14,38 +14,40 @@ class GildedRose
         update_brie(item)
         next
       end
-      if item.name != BACKSTAGE_PASSES
-        if item.quality > 0
-          item.quality = item.quality - 1
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == BACKSTAGE_PASSES
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+      if item.name == BACKSTAGE_PASSES
+        update_backstage_passes(item)
+        next
+      end
+      if item.quality > 0
+        item.quality = item.quality - 1
       end
       item.sell_in = item.sell_in - 1
       if item.sell_in < 0
-        if item.name != BACKSTAGE_PASSES
-          if item.quality > 0
-            item.quality = item.quality - 1
-          end
-        else
-          item.quality = item.quality - item.quality
+        if item.quality > 0
+          item.quality = item.quality - 1
         end
       end
     end
+  end
+end
+
+def update_backstage_passes(item)
+  if item.quality < 50
+    item.quality = item.quality + 1
+    if item.sell_in < 11
+      if item.quality < 50
+        item.quality = item.quality + 1
+      end
+    end
+    if item.sell_in < 6
+      if item.quality < 50
+        item.quality = item.quality + 1
+      end
+    end
+  end
+  item.sell_in = item.sell_in - 1
+  if item.sell_in < 0
+    item.quality = item.quality - item.quality
   end
 end
 
